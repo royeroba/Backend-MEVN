16a83caa14a424fa454d9b153a50667edf57190c
Anibal Royero <132408372+royeroba@users.noreply.github.com>
2025-01-20 12:48:54 -0500

    update credentials

--- models/Services.js
+++ models/Services.js
@@ -1,17 +1,18 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const servicesSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    }, 
-    price: {
-        type: Number,
-        required: true,
-        trim: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+});
 
-const Services = mongoose.model('Services', servicesSchema)
-export default Services
+const Services = mongoose.model("Services", servicesSchema);
+
+export default Services;
